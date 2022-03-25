7bfb30a205507fe41256b629b6892fa3c8e2640b
Suthatta <psuthatta@gmail.com>
2022-03-25 22:11:31 +0100

    Updates

---
+++ precache-manifest.38e5b1329b989f4b82df9ab124f01743.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aed664b814910b907aa",
+    "url": "/myPage/static/css/main.2bad445b.chunk.css"
+  },
+  {
+    "revision": "1aed664b814910b907aa",
+    "url": "/myPage/static/js/main.cb735ee1.chunk.js"
+  },
+  {
+    "revision": "b9fc32a8f2b55818634b",
+    "url": "/myPage/static/js/runtime~main.dd70fbbc.js"
+  },
+  {
+    "revision": "49f57216254a4937231d",
+    "url": "/myPage/static/css/2.2a645d60.chunk.css"
+  },
+  {
+    "revision": "49f57216254a4937231d",
+    "url": "/myPage/static/js/2.d6495e14.chunk.js"
+  },
+  {
+    "revision": "db569b567d12d48e4e61",
+    "url": "/myPage/static/js/3.d51c813c.chunk.js"
+  },
+  {
+    "revision": "31cfe652b9741f85d564aad3afb2d14c",
+    "url": "/myPage/static/media/logo.31cfe652.png"
+  },
+  {
+    "revision": "4fcd624cc978123b1f3be74dc0666017",
+    "url": "/myPage/static/media/Email.4fcd624c.png"
+  },
+  {
+    "revision": "0cdadeb199edbe573aa3713c506702ff",
+    "url": "/myPage/index.html"
+  }
+];
